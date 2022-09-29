c27d755832192e2efd12d792d50a433d0701b72e
notjack <89240346+nootjack@users.noreply.github.com>
2022-09-29 00:11:31 -0700

    Added padding on small screens

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 function Footer(props) {
     return (
-        <div className="dark:bg-stone-800 bg-stone-100 w-full p-10 flex items-center justify-center pt-0 duration-150">
+        <div className="dark:bg-stone-800 bg-stone-100 w-full p-10 flex items-center justify-center pt-5 md:pt-0 duration-150">
             <p className="dark:text-stone-200 text-stone-600 text-xs text-center">
                 {props.children}
             </p>
