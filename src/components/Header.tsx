afcc92101c840d97d2e4dcb526fa66686729449e
notjack <89240346+nootjack@users.noreply.github.com>
2022-12-21 12:52:13 -0800

    Rename Navbar to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
-export default function Navbar() {
+export default function Header() {
   return (
-    <header className="fixed top-0 left-0 flex h-24 w-screen flex-row items-center justify-center gap-4 border-b-2 border-stone-100 py-4 pl-24">
+    <header className="fixed top-0 left-0 flex h-24 w-screen flex-row items-center justify-center gap-4 border-b-2 border-stone-200 py-4 pl-24">
       <nav className="flex w-full justify-between px-8">
         <h1 className="serif text-2xl font-normal italic">Matthew Arthur</h1>
         <ul className="flex items-center justify-between gap-4 text-2xl italic">
