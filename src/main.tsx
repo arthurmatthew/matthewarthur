ef5b1353ffecd9c1e727e0a21ee6b2a9812b102a
notjack <89240346+nootjack@users.noreply.github.com>
2023-08-14 19:42:00 -0700

    Delete everything

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
-// TODO add back strict mode, just gone so useEffect doesn't run twice
-
-createRoot(document.getElementById('root')!).render(<App />);
