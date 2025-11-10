f30c7ab6240c6d81fee27e56ba64df15d61a1b48
notjack <89240346+nootjack@users.noreply.github.com>
2025-11-09 20:26:53 -0800

    refresh website and migrate to astro

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import '../app.css';
-</script>
-
-<div class="w-screen h-screen px-6 flex items-center justify-center">
-	<h1 class="sm:text-5xl text-3xl text-center">
-		This website is currently being rebuilt. Thank you for your patience!
-	</h1>
-</div>
